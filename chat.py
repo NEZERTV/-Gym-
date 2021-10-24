5b1fb345aae4785048fff956b3d59fcbbb5fbd70
NEZERTV <93095237+NEZERTV@users.noreply.github.com>
2021-10-24 22:09:57 +0300

    Gym
    This is gym

--- chat.py
+++ chat.py
@@ -1,1 +1,68 @@
-print ("Welcome to the gym")
+import asyncio
+
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import defer_call, info as session_info, run_async, run_js
+
+chat_msgs = []
+online_users = set()
+
+MAX_MESSAGES_COUNT = 100
+
+async def main():
+    global chat_msgs
+    
+    put_markdown("##  Добро пожаловать в ♂ Gym ♂!\nИсходный ♂ Slave ♂ данного чата укладывается в 100 строк кода!")
+
+    msg_box = output()
+    put_scrollable(msg_box, height=300, keep_bottom=True)
+
+    nickname = await input("Войти в ♂ Gym ♂", required=True, placeholder="Ваше ♂ Gachi ♂ имя", validate=lambda n: "Такой ♂ Gachi ♂ ник уже используется!" if n in online_users or n == '📢' else None)
+    online_users.add(nickname)
+
+    chat_msgs.append(('📢 ♂ Welcome to the club buddy ♂', f'`{nickname}`!'))
+    msg_box.append(put_markdown(f'📢 ♂ Welcome to the club buddy ♂`{nickname}`'))
+
+    refresh_task = run_async(refresh_msg(nickname, msg_box))
+
+    while True:
+        data = await input_group("💭 Новое сообщение", [
+            input(placeholder="Текст сообщения ...", name="msg"),
+            actions(name="cmd", buttons=["Отправить", {'label': "Выйти из ♂ Gym ♂", 'type': 'cancel'}])
+        ], validate = lambda m: ('msg', "Введите текст сообщения!") if m["cmd"] == "Отправить" and not m['msg'] else None)
+
+        if data is None:
+            break
+
+        msg_box.append(put_markdown(f"`{nickname}`: {data['msg']}"))
+        chat_msgs.append((nickname, data['msg']))
+
+    refresh_task.close()
+
+    online_users.remove(nickname)
+    toast("Вы вышли из чата!")
+    msg_box.append(put_markdown(f'📢 Пользователь `{nickname}` покинул ♂ Gym ♂!'))
+    chat_msgs.append(('📢', f'Пользователь `{nickname}` покинул ♂ Gym ♂!'))
+
+    put_buttons(['Перезайти в ♂ Gym ♂'], onclick=lambda btn:run_js('window.location.reload()'))
+
+async def refresh_msg(nickname, msg_box):
+    global chat_msgs
+    last_idx = len(chat_msgs)
+
+    while True:
+        await asyncio.sleep(1)
+        
+        for m in chat_msgs[last_idx:]:
+            if m[0] != nickname: # if not a message from current user
+                msg_box.append(put_markdown(f"`{m[0]}`: {m[1]}"))
+        
+        # remove expired
+        if len(chat_msgs) > MAX_MESSAGES_COUNT:
+            chat_msgs = chat_msgs[len(chat_msgs) // 2:]
+        
+        last_idx = len(chat_msgs)
+
+if __name__ == "__main__":
+    start_server(main, debug=True, port=8080, cdn=False)
